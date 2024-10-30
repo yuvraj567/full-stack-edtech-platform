636a544f83a54e1f26c1fe85e39b6c16674676a4
Yuvraj Singh <98007514+yuvraj567@users.noreply.github.com>
2024-10-30 06:47:10 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ const User = require("../models/User");
 // Configuring dotenv to load environment variables from .env file
 dotenv.config();
 
-// This function is used as middleware to authenticate user requests
+// authenticate user requests
 exports.auth = async (req, res, next) => {
 	try {
 		// Extracting JWT from request cookies, body or header
@@ -14,7 +14,6 @@ exports.auth = async (req, res, next) => {
 			req.body.token ||
 			req.header("Authorization").replace("Bearer ", "");
 
-		// If JWT is missing, return 401 Unauthorized response
 		if (!token) {
 			return res.status(401).json({ success: false, message: `Token Missing` });
 		}
@@ -24,9 +23,9 @@ exports.auth = async (req, res, next) => {
 			const decode = await jwt.verify(token, process.env.JWT_SECRET);
 			console.log(decode);
 			// Storing the decoded JWT payload in the request object for further use
+      //It is important for further verifications and granting access to particular routes.
 			req.user = decode;
 		} catch (error) {
-			// If JWT verification fails, return 401 Unauthorized response
 			return res
 				.status(401)
 				.json({ success: false, message: "token is invalid" });
@@ -35,13 +34,13 @@ exports.auth = async (req, res, next) => {
 		// If JWT is valid, move on to the next middleware or request handler
 		next();
 	} catch (error) {
-		// If there is an error during the authentication process, return 401 Unauthorized response
 		return res.status(401).json({
 			success: false,
 			message: `Something Went Wrong While Validating the Token`,
 		});
 	}
 };
+
 exports.isStudent = async (req, res, next) => {
 	try {
 		const userDetails = await User.findOne({ email: req.user.email });
